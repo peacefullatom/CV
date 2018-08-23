4927edd30aefb352428835162e18f4ed44ce7e53
peacefullatom <peacefullatom@users.noreply.github.com>
2018-08-24 03:33:09 +0500

    added tslint

--- src/app/Const.ts
+++ src/app/Const.ts
@@ -1,22 +1,22 @@
-export const JS = "JavaScript";
-export const TS = "TypeScript";
-export const HTML = "HTML";
-export const CSS = "CSS";
-export const LESS = "LESS";
-export const NODE = "Node.js";
-export const WP = "WebPack";
-export const BABEL = "Babel";
-export const JQ = "jQuery";
-export const A1X = "Angular 1.x";
-export const A2X = "Angular 2.x+";
-export const R = "React";
-export const RN = "React Native";
-export const RX = "Redux";
-export const SQL = "MSSQL";
-export const MD = "MongoDB";
-export const GIT = "Git";
-export const D3 = "D3.js";
-export const ELECTRON = "Electron.js";
+export const JS = 'JavaScript';
+export const TS = 'TypeScript';
+export const HTML = 'HTML';
+export const CSS = 'CSS';
+export const LESS = 'LESS';
+export const NODE = 'Node.js';
+export const WP = 'WebPack';
+export const BABEL = 'Babel';
+export const JQ = 'jQuery';
+export const A1X = 'Angular 1.x';
+export const A2X = 'Angular 2.x+';
+export const R = 'React';
+export const RN = 'React Native';
+export const RX = 'Redux';
+export const SQL = 'MSSQL';
+export const MD = 'MongoDB';
+export const GIT = 'Git';
+export const D3 = 'D3.js';
+export const ELECTRON = 'Electron.js';
 
 export const SECOND = 1E3;
 export const MINUTE = SECOND * 60;
@@ -29,18 +29,20 @@ export const DECADE = YEAR * 10;
 export const CENTURY = YEAR * 1E2;
 export const EON = YEAR * 1E3;
 
-export const MINUTES = "minute(s)";
-export const HOURS = "hour(s)";
-export const DAYS = "day(s)";
-export const MONTHS = "month(s)";
-export const YEARS = "year(s)";
+export const MINUTES = 'minute(s)';
+export const HOURS = 'hour(s)';
+export const DAYS = 'day(s)';
+export const MONTHS = 'month(s)';
+export const YEARS = 'year(s)';
 
-export const MONTHS_LIST = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+export const MONTHS_LIST = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 export const CONVERT_DATE = (date: Date) => {
-    if (!isNaN(date.getTime()))
-        return MONTHS_LIST[date.getMonth()] + " " + date.getFullYear();
+    if (!isNaN(date.getTime())) {
+        return MONTHS_LIST[date.getMonth()] + ' ' + date.getFullYear();
+    }
 
-    return "";
-}
+    return '';
+};
 
-export const CONVERT_LINK = (link: string) => (link.replace(/(^\w+:|^)\/\//, ""));
+export const CONVERT_LINK = (link: string) => (link.replace(/(^\w+:|^)\/\//, ''));
--- src/app/Data.ts
+++ src/app/Data.ts
@@ -30,78 +30,78 @@ export interface Experience extends EntryStart, EntryDescription {
     company: string;
     technologies: string[];
     location: string;
-};
+}
 
 export interface Education extends EntryName, EntryStart, EntryDescription {
     link: string;
     end: Date;
     location: string;
-};
+}
 
 export interface Readings extends EntryName, EntryDescription {
     link: string;
 }
 
 export const DOB = new Date(1981, 11, 9);
-export const LOCATION = "Perm, Russian Federation";
+export const LOCATION = 'Perm, Russian Federation';
 
 export const PHONES: Link[] = [
     {
-        HREF: "tel:+79504696434",
-        value: "+79504696434"
+        HREF: 'tel:+79504696434',
+        value: '+79504696434'
     }
 ];
 
 export const MESSENGERS: Messenger[] = [
     {
-        HREF: "skype:y.i.markov?chat",
-        value: "y.i.markov",
-        messenger: "Skype"
+        HREF: 'skype:y.i.markov?chat',
+        value: 'y.i.markov',
+        messenger: 'Skype'
     },
     {
-        HREF: "tg://resolve?domain=@yuriy_markov",
-        value: "@yuriy_markov",
-        messenger: "Telegram"
+        HREF: 'tg://resolve?domain=@yuriy_markov',
+        value: '@yuriy_markov',
+        messenger: 'Telegram'
     },
     {
-        HREF: "whatsapp://send?phone=79504696434",
-        value: "+79504696434",
-        messenger: "WhatsApp"
+        HREF: 'whatsapp://send?phone=79504696434',
+        value: '+79504696434',
+        messenger: 'WhatsApp'
     },
     {
-        HREF: "viber://chat?number+79504696434",
-        value: "+79504696434",
-        messenger: "Viber"
+        HREF: 'viber://chat?number+79504696434',
+        value: '+79504696434',
+        messenger: 'Viber'
     }
 ];
 
 export const EMAILS: Link[] = [
     {
-        HREF: "mailto:peacefullatom@gmail.com",
-        value: "peacefullatom@gmail.com"
+        HREF: 'mailto:peacefullatom@gmail.com',
+        value: 'peacefullatom@gmail.com'
     },
     {
-        HREF: "mailto:scipios@mail.ru",
-        value: "scipios@mail.ru"
+        HREF: 'mailto:scipios@mail.ru',
+        value: 'scipios@mail.ru'
     }
 ];
 
 export const WEB: Link[] = [
     {
-        HREF: "https://github.com/peacefullatom",
-        value: "GitHub"
+        HREF: 'https://github.com/peacefullatom',
+        value: 'GitHub'
     },
     {
-        HREF: "https://www.instagram.com/peacefullatom",
-        value: "Instagram"
+        HREF: 'https://www.instagram.com/peacefullatom',
+        value: 'Instagram'
     },
     {
-        HREF: "https://twitter.com/markov_yuriy",
-        value: "Twitter"
+        HREF: 'https://twitter.com/markov_yuriy',
+        value: 'Twitter'
     },
     {
-        HREF: "https://fb.me/yurij.markov.9",
-        value: "Facebook"
+        HREF: 'https://fb.me/yurij.markov.9',
+        value: 'Facebook'
     }
 ];
 
@@ -129,8 +129,8 @@ export const SKILLS: Skill[] = [
 
 export const EXPERIENCES: Experience[] = [
     {
-        role: "Senior front-end developer",
-        company: "Foresight LLC",
+        role: 'Senior front-end developer',
+        company: 'Foresight LLC',
         location: LOCATION,
         start: new Date(2017, 3, 17),
         technologies: [
@@ -146,11 +146,14 @@ export const EXPERIENCES: Experience[] = [
             CONST.RX,
             CONST.D3
         ],
-        description: 'Development & support of corporate solution used in BI and analytics, resolving defects found by testers/customers. Development of mobile client on React Native and Redux.'
+        description: [
+            'Development & support of corporate solution used in BI and analytics,',
+            'resolving defects found by testers/customers. Development of mobile client on React Native and Redux.'
+        ].join(' ')
     },
     {
-        role: "Software Developer",
-        company: "SoftM LLC",
+        role: 'Software Developer',
+        company: 'SoftM LLC',
         location: LOCATION,
         start: new Date(2014, 2, 1),
         technologies: [
@@ -160,19 +163,29 @@ export const EXPERIENCES: Experience[] = [
             CONST.JQ,
             CONST.SQL
         ],
-        description: 'Database developer: MS SQL Server 2012. Administrator of mail server. Various tasks of business automation: automated reports generation, reminders system (based on DB and mail server, with web interface) etc. Development and support of CRM.'
+        description: [
+            'Database developer: MS SQL Server 2012. Administrator of mail server.',
+            'Various tasks of business automation: automated reports generation, reminders system',
+            '(based on DB and mail server, with web interface) etc. Development and support of CRM.'
+        ].join(' ')
     },
     {
-        role: "IPTV engineer",
-        company: "WildRed LLC",
-        location: "Sochi, Russian Federation",
+        role: 'IPTV engineer',
+        company: 'WildRed LLC',
+        location: 'Sochi, Russian Federation',
         start: new Date(2014, 1, 1),
-        technologies: ["IPTV"],
-        description: "Worked as IPTV engineer in Bolshoy Ice Dome during Olympic games 2014 on behalf of WildRed LLC. Main tasks were: initial hardware installation, support of system during Olympic games, dismantle of hardware upon event completion, resolving of issues connected with work of system in Bolshoy Ice Dome. Also worked as guide and interpreter for English speaking guests in free time on my own initiative."
+        technologies: ['IPTV'],
+        description: [
+            'Worked as IPTV engineer in Bolshoy Ice Dome during Olympic games 2014 on behalf of WildRed LLC.',
+            'Main tasks were: initial hardware installation, support of system during Olympic games,',
+            'dismantle of hardware upon event completion, resolving of issues connected with work of system',
+            'in Bolshoy Ice Dome. Also worked as guide and interpreter for English speaking guests in free',
+            'time on my own initiative.'
+        ].join(' ')
     },
     {
-        role: "Software Developer",
-        company: "NMS CJSC",
+        role: 'Software Developer',
+        company: 'NMS CJSC',
         location: LOCATION,
         start: new Date(2012, 8, 1),
         technologies: [
@@ -182,11 +195,18 @@ export const EXPERIENCES: Experience[] = [
             CONST.JQ,
             CONST.SQL
         ],
-        description: "Database developer: MS SQL Server 2012. IPTV-interface developer for various set of STBs. Administrator of mail server. Various tasks of business automation: automated reports generation, reminders system (based on DB and mail server, with web interface) etc. Support of incident management system."
+        description: [
+            'Database developer: MS SQL Server 2012.',
+            'IPTV-interface developer for various set of STBs.',
+            'Administrator of mail server.',
+            'Various tasks of business automation: automated reports generation,',
+            'reminders system (based on DB and mail server, with web interface) etc.',
+            'Support of incident management system.'
+        ].join(' ')
     },
     {
-        role: "Network Operations Center Engineer",
-        company: "NMS CJSC",
+        role: 'Network Operations Center Engineer',
+        company: 'NMS CJSC',
         location: LOCATION,
         start: new Date(2008, 3, 1),
         technologies: [
@@ -195,54 +215,71 @@ export const EXPERIENCES: Experience[] = [
             CONST.CSS,
             CONST.SQL
         ],
-        description: "Network administrator: Cisco, Huawei, etc. Mail server administrator. Administrator/developer of MS SQL 2008 database. Partly web developer."
+        description: [
+            'Network administrator: Cisco, Huawei, etc. Mail server administrator.',
+            'Administrator/developer of MS SQL 2008 database. Partly web developer.'
+        ].join(' ')
     },
     {
-        role: "Network Engineer",
-        company: "OKOS LLC",
+        role: 'Network Engineer',
+        company: 'OKOS LLC',
         location: LOCATION,
         start: new Date(2004, 6, 1),
         technologies: [
             CONST.SQL
         ],
-        description: "Development, management, upgrade of network, including VPNs, LANs in big buildings, joints of ISDN, ADSL, Fiber, Ethernet nets. Support of third party software running on Windows and Linux. Management of MS SQL Server 2005: database development, reports generation."
+        description: [
+            'Development, management, upgrade of network, including VPNs, LANs in big buildings,',
+            'joints of ISDN, ADSL, Fiber, Ethernet nets. Support of third party software running',
+            'on Windows and Linux. Management of MS SQL Server 2005: database development, reports generation.'
+        ].join(' ')
     }
 ];
 
 export const EDUCATION: Education[] = [
     {
-        name: "Permskiy Gosudarstvenniy Tekhnologicheskiy Universitet",
-        link: "http://www.pstu.ru",
+        name: 'Permskiy Gosudarstvenniy Tekhnologicheskiy Universitet',
+        link: 'http://www.pstu.ru',
         start: new Date(2002, 8, 1),
         end: new Date(2005, 4, 1),
         location: LOCATION,
-        description: "Control of quality at all stages of product/service life cycle. Specialized in automation of quality control.Attended courses on market analysis, systems of tracking of product/service from development to end user distribution, databases, C/C++ courses, Object-oriented programming."
+        description: [
+            'Control of quality at all stages of product/service life cycle.',
+            'Specialized in automation of quality control.',
+            'Attended courses on market analysis, systems of tracking of product/service',
+            'from development to end user distribution, databases, C/C++ courses, Object-oriented programming.'
+        ].join(' ')
     },
     {
-        name: "Permskiy Khimiko Tekhnologicheskiy Tekhnikum",
-        link: "http://www.phtt.ru",
+        name: 'Permskiy Khimiko Tekhnologicheskiy Tekhnikum',
+        link: 'http://www.phtt.ru',
         start: new Date(1999, 8, 1),
         end: new Date(2002, 4, 1),
         location: LOCATION,
-        description: "Control of quality at all stages of product/service life cycle. Specialized in automation of quality control."
+        description: 'Control of quality at all stages of product/service life cycle. Specialized in automation of quality control.'
     }
 ];
 
 export const READINGS: Readings[] = [
     {
-        name: "Rangle's Angular Training Book",
-        link: "https://angular-2-training-book.rangle.io",
-        description: "One of the best sources about Angular (and not only!) available online. Includes: set of best practices; ready to use solutions; basic recommendations; etc. Written in a very understandable manner, so, the course can be easily understood by novice, but also contains a lot of professional tips."
+        name: 'Rangle\'s Angular Training Book',
+        link: 'https://angular-2-training-book.rangle.io',
+        description: [
+            'One of the best sources about Angular (and not only!) available online.',
+            'Includes: set of best practices; ready to use solutions; basic recommendations; etc.',
+            'Written in a very understandable manner, so, the course can be easily understood',
+            'by novice, but also contains a lot of professional tips.'
+        ].join(' ')
     },
     {
-        name: "Improved Redux type safety with typescript 2.8",
-        link: "https://medium.com/@martin_hotell/improved-redux-type-safety-with-typescript-2-8-2c11a8062575",
-        description: "Type safe actions/reducers/etc with TypeScript! Kudos to Martin Hochel!"
+        name: 'Improved Redux type safety with typescript 2.8',
+        link: 'https://medium.com/@martin_hotell/improved-redux-type-safety-with-typescript-2-8-2c11a8062575',
+        description: 'Type safe actions/reducers/etc with TypeScript! Kudos to Martin Hochel!'
     }
 ];
 
 export const LANGUAGES = [
-    "English",
-    "Russian",
-    "toki pona"
+    'English',
+    'Russian',
+    'toki pona'
 ];
