25637430833994a0494522d87770c69b00630bfe
peacefullatom <peacefullatom@users.noreply.github.com>
2018-09-24 11:27:24 +0500

    add-mongo-support

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@
 
 const configGlobal = require('./global');
 
-// db: `mongodb://localhost/${configGlobal.project}`,
 module.exports = {
-    db: `mongodb+srv://cv_user:cv_pass@scipios-hm1so.mongodb.net/${configGlobal.project}?retryWrites=true`
+    db: `mongodb+srv://cv_user:cv_pass@scipios-hm1so.mongodb.net/${configGlobal.project}`
 };
